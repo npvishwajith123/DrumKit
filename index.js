81e8a2e3e357c9b6e9eace048002d315e3f914ee
npvishwajith123 <npvishwajith@gmail.com>
2024-09-30 12:41:24 +0530

    Added animation

--- index.js
+++ index.js
@@ -10,32 +10,56 @@
 //Approach - 2 (Approach - 1 below)
 
 document.addEventListener("keydown", function(event) {
-        switch(event.key) {
-            case "w":
-                    var crash = new Audio("sounds/crash.mp3");
-                    crash.play();
-            case "a":
-                    var kick = new Audio("sounds/kick-bass.mp3");
-                    kick.play();
-            case "s":
-                    var snare = new Audio("sounds/snare.mp3");
-                    snare.play();
-            case "d":
-                    var tom1 = new Audio("sounds/tom-1.mp3");
-                    tom1.play();
-            case "j":
-                    var tom2 = new Audio("sounds/tom-2.mp3");
-                    tom2.play();
-            case "k":
-                    var tom3 = new Audio("sounds/tom-3.mp3");
-                    tom3.play();
-            case "l":
-                    var tom4 = new Audio("sounds/tom-4.mp3");
-                    tom4.play();
-            default:
-                console.log("Wrong key pressed!!");
-        }
+     playSound(event.key);
+     addAnimation(event.key);   
 });
+
+function playSound(event) {
+    switch(event) {
+        case "w":
+                var crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+        case "a":
+                var kick = new Audio("sounds/kick-bass.mp3");
+                kick.play();
+                break;
+        case "s":
+                var snare = new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+        case "d":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+        case "j":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+        case "k":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+        case "l":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+        default:
+            console.log("Wrong key pressed!!");
+            break;
+    }
+}
+
+function addAnimation(key) {
+    console.log(key);
+    var activeButton  = document.querySelector("."+key);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
+}
+
 
 //Approach - 1
 
@@ -45,36 +69,43 @@ for(var i=0; i<buttons.length; i++) {
     if(buttons[i].classList.contains("w")) {
         buttons[i].addEventListener("click", function () {
             var audio = new Audio("sounds/crash.mp3");
+            addAnimation("w");
             audio.play();
         });
     } else if(buttons[i].classList.contains("a")) {
         buttons[i].addEventListener("click", function () {
             var audio = new Audio("sounds/kick-bass.mp3");
+            addAnimation("a");
             audio.play();
         });
     } else if(buttons[i].classList.contains("s")) {
         buttons[i].addEventListener("click", function () {
             var audio = new Audio("sounds/snare.mp3");
+            addAnimation("s");
             audio.play();
         });
     } else if(buttons[i].classList.contains("d")) {
         buttons[i].addEventListener("click", function () {
             var audio = new Audio("sounds/tom-1.mp3");
+            addAnimation("d");
             audio.play();
         });
     } else if(buttons[i].classList.contains("j")) {
         buttons[i].addEventListener("click", function () {
             var audio = new Audio("sounds/tom-2.mp3");
+            addAnimation("j");
             audio.play();
         });
     } else if(buttons[i].classList.contains("k")) {
         buttons[i].addEventListener("click", function () {
             var audio = new Audio("sounds/tom-3.mp3");
+            addAnimation("k");
             audio.play();
         });
     } else if(buttons[i].classList.contains("l")) {
         buttons[i].addEventListener("click", function () {
             var audio = new Audio("sounds/tom-4.mp3");
+            addAnimation("l");
             audio.play();
         });
     }  
